330616745818419777e55e914f67815f53f3f07c
KrishnenduNair <84639722+KrishnenduNair@users.noreply.github.com>
2026-04-07 11:08:49 +0530

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        curr_win, max_win = 0, 0
+        window = s[0]
+        for i in range(1, len(s)):
+            if s[i] not in window:
+                window += s[i]
+            else:
+                curr_window = len(window)
+                max_win = max(max_win, curr_win)
+                window = window[window.index(s[i])+1:]
+                window += s[i]
+            max_win = max(max_win, len(window))
+        return max_win           
+            
+            

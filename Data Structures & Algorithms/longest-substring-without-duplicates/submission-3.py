a06439926e12218294af218c340a7d4bff11209d
KrishnenduNair <84639722+KrishnenduNair@users.noreply.github.com>
2026-04-07 11:10:39 +0530

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s:
+            return 0
+        curr_win, max_win = 0, 0
+        window = s[0]
+        for i in range(1, len(s)):
+            if s[i] not in window:
+                window += s[i]
+            else:
+                curr_window = len(window)
+                max_win = max(max_win, curr_win)
+                window = window[window.index(s[i])+1:]
+                window += s[i]
+            max_win = max(max_win, len(window))
+        return max_win           
+            
+            

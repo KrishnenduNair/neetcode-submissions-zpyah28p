ad92619bbdc7d7798773fd0a41d1ae380411d0d9
Krishnendu Nair <84639722+KrishnenduNair@users.noreply.github.com>
2026-08-12 08:56:48 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+from bisect import bisect_right
+
+class TimeMap:
+
+    def __init__(self):
+        self.store = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        return self.store[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.store:
+            return ""
+        values = self.store[key]
+        idx = bisect_right(values, (timestamp, chr(127)))
+        if idx == 0:
+            return ""
+        return values[idx-1][1]
+
+        
